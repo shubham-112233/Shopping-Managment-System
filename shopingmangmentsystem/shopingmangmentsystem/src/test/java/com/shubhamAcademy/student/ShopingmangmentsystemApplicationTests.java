62e92b1f23b176ee7efc1c52a8512a9c91e8482c
shubham kolhe <131533840+shubham-112233@users.noreply.github.com>
2025-04-07 15:29:53 +0530

    Add files via upload

---
+++ shopingmangmentsystem/shopingmangmentsystem/src/test/java/com/shubhamAcademy/student/ShopingmangmentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhamAcademy.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingmangmentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
